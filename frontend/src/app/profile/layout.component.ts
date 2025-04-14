a133165bfd22ffb7472ad85b40be0e295cd708d6
AmaroKobe03 <148180353+AmaroKobe03@users.noreply.github.com>
2025-04-14 22:23:23 +0800

    Initial Commits - need dependency fixing

---
+++ frontend/src/app/profile/layout.component.ts
@@ -0,0 +1,4 @@
+import { Component } from '@angular/core';
+
+@Component({ templateUrl: 'layouot.component.html'})
+export class LayoutComponent {}
---
+++ frontend/src/polyfills.ts
@@ -0,0 +1,6 @@
+/*
+ * Polyfills
+ */
+
+// import 'zone.js/dist/zone';  // Included with Angular CLI.
+import 'zone.js';
